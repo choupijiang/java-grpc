72253b1b7d36a72b75700d49a1aa6658642c039b
zhangshenghu <zhangshenghu@sensetime.com>
2021-10-17 11:39:39 +0800

    test

--- grpc/src/main/java/tech/choupijiang/grpc/server/HelloWorldServiceImpl.java
+++ grpc/src/main/java/tech/choupijiang/grpc/server/HelloWorldServiceImpl.java
@@ -6,17 +6,25 @@ import com.codenotfound.grpc.helloworld.Person;
 import io.grpc.stub.StreamObserver;
 import lombok.extern.log4j.Log4j2;
 import org.lognet.springboot.grpc.GRpcService;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import tech.choupijiang.grpc.control.TestController;
 
 
 @GRpcService
 @Log4j2
 public class HelloWorldServiceImpl extends HelloWorldServiceGrpc.HelloWorldServiceImplBase {
 
+
+    @Autowired
+    TestController controller;
+
+
     @Override
     public void sayHello(Person request,
                          StreamObserver<Greeting> responseObserver) {
         log.info("server received {}", request);
+
+        System.out.println(controller.test());
 
         String message = "Hello " + request.getFirstName() + " "
                 + request.getLastName() + "!";
